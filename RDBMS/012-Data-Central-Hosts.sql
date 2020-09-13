d6fbffccad7bc81e52383a0aed180f97220d6b2a
Saradhi Vemuri <saradhivs@yahoo.com>
2020-09-13 10:25:44 -0400

    Fixed Syntax errors.

--- RDBMS/012-Data-Central-Hosts.sql
+++ RDBMS/012-Data-Central-Hosts.sql
@@ -1,14 +1,14 @@
 DELETE FROM Hosts;
 
 #TODO Better classification and Description everything is a (server/Desktop/Loadbalancer?) / Messaging-RabbitMQ
-INSERT INTO Hosts (Environment, Type, Purpose, HostName) VALUES
-('DEV', 'Server', 'SVN', 'ec2-54-209-149-104.compute-1.amazonaws.com'),
-('DEV', 'Server', 'Messaging', 'ec2-54-172-250-220.compute-1.amazonaws.com'),
-('DEV', 'Server', 'Database', 'elektos.cty8sfgs1evu.us-east-1.rds.amazonaws.com'),
-('DEV', 'Server', 'Central-1', 'ec2-52-23-185-3.compute-1.amazonaws.com'),
+INSERT INTO Hosts (Environment, Type, Purpose, HostName, Description) VALUES
+('DEV', 'Server', 'SVN', 'ec2-54-209-149-104.compute-1.amazonaws.com', 'Source Control'),
+('DEV', 'Server', 'Messaging', 'ec2-54-172-250-220.compute-1.amazonaws.com', 'Messaging Server currently only RabbitMQ'),
+('DEV', 'Server', 'Database', 'elektos.cty8sfgs1evu.us-east-1.rds.amazonaws.com', 'MySql Database'),
+('DEV', 'Server', 'CentralServer', 'ec2-52-23-185-3.compute-1.amazonaws.com', 'Single instance of Central Server'),
 
 #WHAT exactly is this entry for?
-('DEV', 'Server', 'Central-1', '172.31.35.139'),
+('DEV', 'Server', 'CentralServer', '172.31.35.139', 'IP Address of Central Server when called internally'),
 
 #WHY is this entry here?
 ('DEV', 'Desktop', 'Personal', '127.0.0.1', 'Hostname looks like an IP Address but that is what is returned by HttpServletRequest.getRemoteHost().'),
