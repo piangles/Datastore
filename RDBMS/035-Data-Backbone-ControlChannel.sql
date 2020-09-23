9e6aac6528a4b78e3870998a9ea68168dbd1b382
saradhivs <saradhivs@yahoo.com>
2020-09-22 20:20:46 -0400

    Init

--- RDBMS/035-Data-Backbone-ControlChannel.sql
+++ RDBMS/035-Data-Backbone-ControlChannel.sql
@@ -2,6 +2,6 @@ USE Backbone;
 DELETE FROM ControlChannel;
 
 INSERT INTO ControlChannel (TypeName, TypeValue, Topic) VALUES
-('UserId', '7014b086', 'com.TBD.controlchannel.7014b086'),
+('UserId', '7014b086', 'com.TBD.controlchannel.users'),
 ('Alias', 'Energy', 'com.TBD.sector.energy'),
 ('Alias', 'Technology', 'com.TBD.sector.tech');
