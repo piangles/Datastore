f9ec5a1e68557576ca19e754a10e5415a27c7153
saradhivs <saradhivs@yahoo.com>
2020-11-20 06:10:34 -0500

    Added Mongo related config.

--- RDBMS/025-Data-Backbone-LoggingConfig.sql
+++ RDBMS/025-Data-Backbone-LoggingConfig.sql
@@ -2,12 +2,21 @@ USE Backbone;
 DELETE FROM Config WHERE ComponentId IN ('14fe64ea-d15a-4c8b-af2f-f2c7efe1943b');
 
 INSERT INTO Config (ApplicationId, ComponentId, ComponentDescription, Name, Value) VALUES
-('LoggingService', '14fe64ea-d15a-4c8b-af2f-f2c7efe1943b', 'LoggingServiceDAO', 'DriverClassName', 'com.mysql.jdbc.Driver'),
-('LoggingService', '14fe64ea-d15a-4c8b-af2f-f2c7efe1943b', 'LoggingServiceDAO', 'LoginId', 'kj5IPCk6oWkmxezE9yWh3Q=='),
+#('LoggingService', '14fe64ea-d15a-4c8b-af2f-f2c7efe1943b', 'LoggingServiceDAO', 'DriverClassName', 'com.mysql.jdbc.Driver'),
+#('LoggingService', '14fe64ea-d15a-4c8b-af2f-f2c7efe1943b', 'LoggingServiceDAO', 'LoginId', 'kj5IPCk6oWkmxezE9yWh3Q=='),
+#('LoggingService', '14fe64ea-d15a-4c8b-af2f-f2c7efe1943b', 'LoggingServiceDAO', 'Password', 'bSU2ekVvuinOQTa/ywH7kg=='),
+#('LoggingService', '14fe64ea-d15a-4c8b-af2f-f2c7efe1943b', 'LoggingServiceDAO', 'URL', 'jdbc:mysql://elektos.cty8sfgs1evu.us-east-1.rds.amazonaws.com:3306/'),
+#('LoggingService', '14fe64ea-d15a-4c8b-af2f-f2c7efe1943b', 'LoggingServiceDAO', 'DecrypterClassName', 'org.piangles.backbone.services.crypto.DefaultDecrypter'),
+#('LoggingService', '14fe64ea-d15a-4c8b-af2f-f2c7efe1943b', 'LoggingServiceDAO', 'DecrypterAuthorizationId', 'e04202bc-a88e-48be-8b38-b94399d3086a');
+
+('LoggingService', '14fe64ea-d15a-4c8b-af2f-f2c7efe1943b', 'LoggingServiceDAO', 'Type', 'Mongo'),
+('LoggingService', '14fe64ea-d15a-4c8b-af2f-f2c7efe1943b', 'LoggingServiceDAO', 'LoginId', 'wyRvtg/TqZoNR9Gpxsk5Cg=='),
 ('LoggingService', '14fe64ea-d15a-4c8b-af2f-f2c7efe1943b', 'LoggingServiceDAO', 'Password', 'bSU2ekVvuinOQTa/ywH7kg=='),
-('LoggingService', '14fe64ea-d15a-4c8b-af2f-f2c7efe1943b', 'LoggingServiceDAO', 'URL', 'jdbc:mysql://elektos.cty8sfgs1evu.us-east-1.rds.amazonaws.com:3306/'),
+('LoggingService', '14fe64ea-d15a-4c8b-af2f-f2c7efe1943b', 'LoggingServiceDAO', 'HostAddress', 'ec2-34-202-160-243.compute-1.amazonaws.com:27017'),
+('LoggingService', '14fe64ea-d15a-4c8b-af2f-f2c7efe1943b', 'LoggingServiceDAO', 'Database', 'piangles'),
 ('LoggingService', '14fe64ea-d15a-4c8b-af2f-f2c7efe1943b', 'LoggingServiceDAO', 'DecrypterClassName', 'org.piangles.backbone.services.crypto.DefaultDecrypter'),
 ('LoggingService', '14fe64ea-d15a-4c8b-af2f-f2c7efe1943b', 'LoggingServiceDAO', 'DecrypterAuthorizationId', 'e04202bc-a88e-48be-8b38-b94399d3086a');
+
 
 
 #DELETE FROM Config WHERE ComponentId IN ('24765f79-834a-4761-9180-4e0450e6020b', '632cc560-841f-4551-bac6-232102ef5503');
