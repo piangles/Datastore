4e68a52d1f56a4aa3cefab404eaf78dd2d155aad
saradhivs <saradhivs@yahoo.com>
2020-12-28 17:39:37 -0500

    renamed container.

--- Postgres/restartPostgres.sh
+++ Postgres/restartPostgres.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker stop piangles
+docker stop piangles_db
 
-docker image build -t piangles .
+docker image build -t piangles_db .
 
-docker container run -p 5432:5432 --rm -it --name piangles piangles 
+docker container run -p 5432:5432 --rm -it --name piangles_db piangles_db 
 
 sleep 5
 
