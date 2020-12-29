0f7834446169058438dc7aaa5e6319c6175df34f
saradhivs <saradhivs@yahoo.com>
2020-12-28 20:14:15 -0500

    Fixed syntax error.

--- Postgres/DML/Central-GetDiscoveryProperties.sql
+++ Postgres/DML/Central-GetDiscoveryProperties.sql
@@ -10,11 +10,11 @@ AS $$
 BEGIN
     
 	--Check if the Host is authorized need a separate point or return value.
-	SELECT Environment INTO Environment FROM Central.Hosts hosts WHERE hosts.HostName=HostName;
+	SELECT hosts.Environment INTO Environment FROM Central.Hosts hosts WHERE hosts.HostName=HostName;
 
 	--call DebugLog(Environment);
 	
 	--If Properties are not set handle it.
-    SELECT Name, Value FROM Central.Discovery discovery WHERE discovery.Environment = Environment and discovery.ServiceName = ServiceName;
+    SELECT discovery.Name, discovery.Value FROM Central.Discovery discovery WHERE discovery.Environment = Environment and discovery.ServiceName = ServiceName;
 END
 $$ LANGUAGE plpgsql;
