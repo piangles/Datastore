2eef3c3466129e14fda0cc2b7442ca58ebeedfa5
saradhivs <saradhivs@yahoo.com>
2020-12-28 21:18:49 -0500

    Fixed syntax error.

--- Postgres/DML/Central-GetTier1Configuration.sql
+++ Postgres/DML/Central-GetTier1Configuration.sql
@@ -1,10 +1,11 @@
-DROP PROCEDURE IF EXISTS Central.GetTier1Configuration;
+DROP FUNCTION IF EXISTS Central.GetTier1Configuration;
 
-CREATE PROCEDURE Central.GetTier1Configuration 
+CREATE FUNCTION Central.GetTier1Configuration 
 (
 	IN pHostName VARCHAR(255),
 	IN pServiceName VARCHAR(255)
 ) 
+RETURNS TABLE (Name VARCHAR(250), Value VARCHAR(1000))
 AS $$
 	DECLARE vEnvironment VARCHAR(3);
 BEGIN
@@ -12,7 +13,9 @@ BEGIN
 	SELECT hosts.Environment INTO vEnvironment FROM Central.Hosts hosts WHERE hosts.HostName=pHostName;
 
 	--call DebugLog(Environment);
-	
-    SELECT Name, Value FROM Central.Tier1Configuration tier1Config WHERE tier1Config.Environment = vEnvironment and tier1Config.ServiceName = pServiceName;
+	RETURN QUERY
+    SELECT tier1Config.Name, tier1Config.Value FROM Central.Tier1Configuration tier1Config 
+    WHERE 
+    tier1Config.Environment = vEnvironment and tier1Config.ServiceName = pServiceName;
 END
 $$ LANGUAGE plpgsql;
