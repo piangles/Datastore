28c132298fdc52786961d80dc075f5cac0301aa4
EC2 Default User <ec2-user@ip-172-31-57-71.ec2.internal>
2020-12-30 03:12:45 +0000

    Title
    Description...Regenerating init.sql

--- Postgres/init.sql
+++ Postgres/init.sql
@@ -277,17 +277,17 @@ DROP PROCEDURE IF EXISTS Backbone.CreateUserProfile;
 
 CREATE PROCEDURE Backbone.CreateUserProfile
 (
-	IN UserId VARCHAR(25),
-	IN FirstName VARCHAR(50),
-	IN LastName VARCHAR(50),
-	IN EMailId VARCHAR(250)
-	IN PhoneNo VARCHAR(25)
+	IN pUserId VARCHAR(25),
+	IN pFirstName VARCHAR(50),
+	IN pLastName VARCHAR(50),
+	IN pEMailId VARCHAR(250),
+	IN pPhoneNo VARCHAR(25)
 ) 
 AS $$
 BEGIN
 
 	INSERT INTO Backbone.UserProfile (UserId, FirstName, LastName, EMailId, PhoneNo)
-	VALUES (UserId, FirstName, LastName, EMailId, PhoneNo);
+	VALUES (pUserId, pFirstName, pLastName, pEMailId, pPhoneNo);
     
 END
 $$ LANGUAGE plpgsql;
